907d63a59572c173f651a52ed4e6fd6a6e7e2ad0
Radosław Bucki <236507@edu.p.lodz.pl>
2023-03-22 19:43:12 +0100

    Java 15

--- modern-java-features/src/main/java/java15/record/Car.java
+++ modern-java-features/src/main/java/java15/record/Car.java
@@ -3,18 +3,10 @@ package java15.record;
 // implement here
 // change this class to record
 // throw IllegalArgumentException when capacity in negative while creating
-public class Car {
-
-
-    public Car(String make, int capacity) {
-
-    }
-
-    public String make() {
-        return "";
-    }
-
-    public int capacity() {
-        return 0;
+public record Car(String make, int capacity) {
+    public Car {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity cannot be negative");
+        }
     }
 }
--- modern-java-features/src/main/java/java15/textBlock/HtmlCode.java
+++ modern-java-features/src/main/java/java15/textBlock/HtmlCode.java
@@ -5,15 +5,17 @@ public class HtmlCode {
         // implement here
         // rewrite using text block
 
-        return "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "  <head>\n" +
-                "    <meta charset=\"utf-8\">\n" +
-                "    <title>My test page</title>\n" +
-                "  </head>\n" +
-                "  <body>\n" +
-                "    <img src=\"images/firefox-icon.png\" alt=\"My test image\">\n" +
-                "  </body>\n" +
-                "</html>\n";
+        return """
+                <!DOCTYPE html>
+                <html>
+                  <head>
+                    <meta charset="utf-8">
+                    <title>My test page</title>
+                  </head>
+                  <body>
+                    <img src="images/firefox-icon.png" alt="My test image">
+                  </body>
+                </html>
+                """;
     }
 }
